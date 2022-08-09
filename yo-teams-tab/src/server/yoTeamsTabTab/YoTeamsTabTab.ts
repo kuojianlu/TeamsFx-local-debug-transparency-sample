bafe66f0c8ce6e51a1afcb13b20b207b102e78c4
Kuojian Lu <kuojianlu@gmail.com>
2022-08-09 17:52:16 +0800

    add yo teams tab project

---
+++ yo-teams-tab/src/server/yoTeamsTabTab/YoTeamsTabTab.ts
@@ -0,0 +1,8 @@
+import { PreventIframe } from "express-msteams-host";
+
+/**
+ * Used as place holder for the decorators
+ */
+@PreventIframe("/yoTeamsTabTab/index.html")
+export class YoTeamsTabTab {
+}
